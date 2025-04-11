54b34fade4e19f383d4f1053673c8695efe84a7b
parrotcode <parrotbaobao@gmail.com>
2025-04-12 00:16:56 +0800

    git

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,6 +21,8 @@ const config: Config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  deploymentBranch: 'master',
+
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
